8e870021f6e1f82b1aaa94cabc9c5c186417a37d
97Chanuk <chanuk9531@daum.net>
2023-03-30 21:17:21 +0900

    Implement bubble sort
    버블 소트와 테스트 구현

---
+++ fastcampus-java-oop/src/main/java/org/fastcampus/logic/BubbleSort.java
@@ -0,0 +1,23 @@
+package org.fastcampus.logic;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BubbleSort <T extends Comparable<T>>{
+
+    public List<T> sort(List<T> input){
+        List<T> output = new ArrayList<>(input);
+
+        for(int i=0; i< output.size(); i++){
+            for(int j=0; j<i; j++){
+                T x = output.get(i);
+                T y = output.get(j);
+                if(x.compareTo(y) < 0){
+                    output.set(i,y);
+                    output.set(j,x);
+                }
+            }
+        }
+        return output;
+    }
+}
