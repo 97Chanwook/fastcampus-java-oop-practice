86857d4a1230886598a1dc8f4e3c9938f6c1b625
97Chanuk <chanuk9531@daum.net>
2023-03-30 21:55:10 +0900

    JavaSort 수정
    Input 안받음

--- fastcampus-java-oop/src/main/java/org/fastcampus/logic/JavaSort.java
+++ fastcampus-java-oop/src/main/java/org/fastcampus/logic/JavaSort.java
@@ -9,7 +9,7 @@ public class JavaSort <T extends Comparable<T>> implements Sort<T>{
 
     @Override
     public List<T> sort(List<T> input){
-        List<T> output = new ArrayList<>();
+        List<T> output = new ArrayList<>(input);
         Collections.sort(output);
         return output;
     }
