bec1523eb898789ad8b5f6a346733601f1e65326
97Chanuk <chanuk9531@daum.net>
2023-03-30 21:41:03 +0900

    Design #4 - Sort Interface
    인터페이스 설계하고, 각 구현체가 인터페이스를 구현하는 구조로 수정

--- fastcampus-java-oop/src/main/java/org/fastcampus/logic/JavaSort.java
+++ fastcampus-java-oop/src/main/java/org/fastcampus/logic/JavaSort.java
@@ -5,8 +5,9 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 
-public class JavaSort <T extends Comparable<T>>{
+public class JavaSort <T extends Comparable<T>> implements Sort<T>{
 
+    @Override
     public List<T> sort(List<T> input){
         List<T> output = new ArrayList<>();
         Collections.sort(output);
